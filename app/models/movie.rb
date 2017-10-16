a8ba5382d641a3320104a71778d25dde629e7fcd
Dichha Rai <dichha-rai@uiowa.edu>
2017-10-16 04:46:37 +0000

    refactored finding rating for finding using movie id

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ActiveRecord::Base
   def self.all_ratings
-    %w(G PG PG-13 NC-17 R)
+    %w(G PG PG-13 NC-17 R NR)
   end
   
 #class Movie::InvalidKeyError < StandardError ; end
@@ -18,7 +18,7 @@ class Movie < ActiveRecord::Base
     # movie_parameters = self.find_in_tmdb(new_m.title)
     # movie_parameters.each do |pairs|
     #   pairs.each do |key, val|
-    #     if key == ":rating"
+    #     if key == ":rating" 
     #       new_m.rating = val
     #     end
     #   end
@@ -28,18 +28,35 @@ class Movie < ActiveRecord::Base
     new_m.title = tmdb_m["title"]
     new_m.description = tmdb_m["overview"]
     new_m.release_date = tmdb_m["release_date"]
-    movie_parameters = self.find_in_tmdb(new_m.title)
-    movie_parameters.each do |pairs|
-      pairs.each do |key, val|
-        if key == ":rating"
-          new_m.rating = val
-        end
-      end
-    end
+    new_m.rating = self.find_ratings(movie_id)
+    # movie_parameters = self.find_in_tmdb(new_m.title)
+    # movie_parameters.each do |pairs|
+    #   pairs.each do |key, val|
+    #     if (key == ":rating") &&
+    #       new_m.rating = val
+    #     end
+    #   end
+    # end
   new_m.save
     
   end
-    
+  def self.find_ratings(movie_id)
+    releases = Tmdb::Movie.releases(movie_id)
+    rating = "NR"
+    if !releases.nil?
+      release_country = releases["countries"]
+      if !release_country.nil? #|| !release_country.empty?
+        release_country.each do |rate|
+          if rate["iso_3166_1"] == "US"
+            if !rate["certification"].empty? 
+              rating = rate["certification"]
+            end
+          end
+        end
+      end
+    end
+    rating
+  end
 
   def self.find_in_tmdb(keyword)
     Tmdb::Api.key("f4702b08c0ac6ea5b51425788bb26562")
@@ -47,20 +64,21 @@ class Movie < ActiveRecord::Base
     matching_movies = Tmdb::Movie.find(keyword)
     if !matching_movies.nil? || !matching_movies.empty?
       matching_movies.each do |movie|
-        releases = Tmdb::Movie.releases(movie.id)
-        rating = "NR"
-        if !releases.nil?
-          release_country = releases["countries"]
-          if !release_country.nil? #|| !release_country.empty?
-            release_country.each do |rate|
-              if rate["iso_3166_1"] == "US"
-                if !rate["certification"].empty? 
-                  rating = rate["certification"]
-                end
-              end
-            end
-          end
-        end
+        rating = self.find_ratings(movie.id)
+        # releases = Tmdb::Movie.releases(movie.id)
+        # rating = "NR"
+        # if !releases.nil?
+        #   release_country = releases["countries"]
+        #   if !release_country.nil? #|| !release_country.empty?
+        #     release_country.each do |rate|
+        #       if rate["iso_3166_1"] == "US"
+        #         if !rate["certification"].empty? 
+        #           rating = rate["certification"]
+        #         end
+        #       end
+        #     end
+        #   end
+        # end
         #release_country_us_rating = releases["countries"][0]["certification"]
         movies.push({":id"=>"#{movie.id}",":title"=>"#{movie.title}",":rating"=>"#{rating}",":release_date"=>"#{movie.release_date}"})
       end
